d5b8f183ee6b6fc7ffdfd883c3b9ab720225bd12
n132 <xm2146@nyu.edu>
2022-06-29 15:32:45 -0400

    Almost Done

--- CryPtown/chal/main.c
+++ CryPtown/chal/main.c
@@ -3,7 +3,7 @@
 #include<fcntl.h>
 #include<stdlib.h>
 #include<unistd.h>
-#include<utils.c>
+#include<string.h>
 double R = 0.32;
 size_t L = 0x400;
 
@@ -35,41 +35,35 @@ uint8_t urand_byte(int f)
     return r;    
 }
 // ------------------------------------------------------------
-
 typedef struct random_bytes
 {
     size_t *pos;
     size_t cur;
     size_t limit; 
 } random_bytes; 
-
 typedef struct key_struct
 {
     random_bytes *rb;
     // random character positions 2**16 may result uncontrolable result
     uint8_t  *key;
     size_t key_len;
-    // a string
 } key_struct; 
 
-key_struct KList[0x10];
-
-void * secure_malloc(size_t size)
-{
+key_struct *KList[0x10];
+// ------------------------------------------------------------
+void * secure_malloc(size_t size){
     void * p = malloc(size);
     if(p<=0)
         panic("Malloc error");
     return p ; 
 }
-void * secure_realloc(void * p, size_t size)
-{
+void * secure_realloc(void * p, size_t size){
     p = realloc(p,size);
     if(p<=0)
         panic("Realloc error");
     return p ; 
 }
-key_struct * key_init(uint8_t *key,size_t kl)
-{
+key_struct * key_init(uint8_t *key,size_t kl){
     key_struct *k = 0 ;
     k       = secure_malloc(sizeof(key_struct));
     k->rb   = secure_malloc(sizeof(random_bytes));
@@ -80,23 +74,29 @@ key_struct * key_init(uint8_t *key,size_t kl)
     k->key_len = kl;
     return k; 
 }
-void enc(uint8_t *plaintext, key_struct * key, uint8_t *key, size_t key_len)
-{
+void enc(uint8_t *plaintext, size_t plaintext_len , key_struct * k){
     int f       = 0;
     uint8_t * p = 0;
     size_t ct   = 0;
     size_t rct  = 0;
     uint8_t * e = 0;
-    
+    size_t c_len = plaintext_len;
+
     f   = open("/dev/urandom",0,0);
     p   = plaintext; 
-    e   = secure_realloc(0,L);
-    if( f<0)
-        panic("enc");
-
-    key_struct *k = key_init(key,key_len);
+    e   = secure_realloc(0, c_len);
     
-    while( p[ct-rct]!= 0)
+    if(f<0)
+        panic("Open");
+    if(k->rb)
+    {
+        if(k->rb->pos)
+            free(k->rb->pos);
+        free(k->rb);
+        k->rb = secure_malloc(sizeof(random_bytes));
+        memset(k->rb,0,sizeof(random_bytes));
+    }
+    while( ct-rct != plaintext_len )
     {
         if(urand_byte(f)<= R * 0x100)
         {
@@ -108,36 +108,60 @@ void enc(uint8_t *plaintext, key_struct * key, uint8_t *key, size_t key_len)
             }
             k->rb->cur++;
             k->rb->pos[rct++] = ct ;
-            e[ct++] = urand_byte(f); // Insert a random byte
+            e[ct++] = urand_byte(f); // append a random byte
         }
         else{
-            e[ct++] = k->key[(ct-rct)%key_len] ^ p[ct-rct];
+            e[ct++] = k->key[(ct-rct)%(k->key_len)] ^ p[ct-rct];
+        }
+        
+        if(ct>=c_len)
+        {
+            if(ct>=4*plaintext_len)
+                panic("Meaningless Setting");
+            e = secure_realloc(e,c_len*2);
+            c_len = c_len*2 ; 
         }
     }
     close(f);
-    for(int i =0 ; i < ct;i++)
+    #ifdef DEBUG
+        for(int i =0 ; i < ct;i++)
+        {
+            printf("%d\n",e[i]);
+        }
+        return;
+    #endif
+    write(1,e,ct);
+    free(e);
+}
+void dec(uint8_t *ciphertext, size_t c_len,key_struct *key){
+    size_t ct  = 0 ; 
+    size_t kl = key->key_len;
+    uint8_t * key_string = key->key;
+    for(int i=0;i<c_len;i++)
     {
-        printf("%d\n",e[i]);
+        if( ct < key->rb->cur);
+        {
+            if(i==key->rb->pos[ct])
+            {
+                ct++;
+                continue;
+            }
+        }
+        uint8_t n = ciphertext[i] ^ key_string[(i-ct)%kl];
+        write(1,&n,1);
     }
 }
-void dec(uint8_t *ciphertext,key_struct *key)
-{
-    printf("TODO");
-}
-void logo()
-{
+void logo(){
     ;
 }
-void init()
-{
+void init(){
     fclose(stderr);
     setvbuf(stdin, 0LL, 2, 0LL);
     setvbuf(stdout, 0LL, 2, 0LL);
     alarm(120);//I'll set the alarm in set-up.sh so I can remove this.
     logo();
 }
-void menu()
-{
+void menu(){
     puts("=========================");
     puts("0. Key Management");
     puts("1. Encode");
@@ -147,14 +171,20 @@ void menu()
     puts(" ========================= ");
 }
 void key_list(){
+    int tmp = 1 ; 
     puts(" ========================= ");
+    
     for(int i = 0 ; i<0x10; i++)
     {
         if(KList[i])
         {
-            printf(" Key[ %d ], len = %d \n",i,KList[i].key_len);
+            printf(" Key[ %d ], len = %lu \n",i,KList[i]->key_len);
+            tmp = 0;
         }
     }
+    if(tmp==1)
+        puts("\t\tNone");
+
     puts(" ========================= ");
 }
 int key_insert(key_struct * k)
@@ -202,7 +232,7 @@ void key_gen(){
     uint8_t * key = secure_malloc(key_len) ;
     printf("Key string: ");
     readn(key,key_len);
-    key_struct k = key_init(key,key_len);
+    key_struct *k = key_init(key,key_len);
     if(key_insert(k)){
         puts("[-] Fail to Gen a New Key");
         key_destructor(k);
@@ -212,15 +242,14 @@ void key_gen(){
     }
 }
 void key_menu(){
-    puts("=========================");
+    puts(" =========================");
     puts("0. Add a Key");
     puts("1. Del a Key");
     puts("2. Show all Keys");
     puts("3. Return");
     puts(" ========================= ");
 }
-void key_management()
-{
+void key_management(){
     while(1)
     {
         key_menu();
@@ -248,9 +277,38 @@ void encode(){
         panic("Invalid data");
     if(!KList[idx])
         panic("Invalid data");
+    printf("The length of your plaintext: ");
+    size_t p_len = readint();
+    if(!p_len)
+        return ;
+    if(p_len>0x1000)
+        panic("Too long to keep it secure"); //This is actually a hint!
+    uint8_t *plaintext  = (uint8_t *)secure_malloc(p_len);
+    readn(plaintext,p_len);
+    enc(plaintext,p_len,KList[idx]);
+    puts("[+] Encode Done");
 }
-void decode(){;}
-void challge(){;}
+void decode(){
+    key_list();
+    printf("Which key do you want to use: ");
+    size_t idx = readint();
+    if(idx >= 0x10)
+        panic("Invalid data");
+    if(KList[idx]==0 || KList[idx]->rb==0 || KList[idx]->rb->pos==0)
+        panic("Invalid data");
+    printf("The length of your ciphertext: ");
+    size_t c_len = readint();
+    if(c_len>0x2000 || c_len == 0)
+        panic("Invalid data");
+    uint8_t *c = secure_malloc(c_len);
+    readn(c,c_len);
+    dec(c,c_len,KList[idx]);
+    puts("[+] Decode Done");
+}
+void challge(){
+    ;
+    //todo
+}
 void play_ground()
 {
     while(1)
@@ -276,9 +334,10 @@ void play_ground()
     }
 }
 void test(){
-    uint8_t p[]= {1,2,3,4,5,6,67};
-    uint8_t k[]= {0};
-    enc(p,k,sizeof(k));
+    ;
+    // uint8_t p[]= {1,2,3,4,5,6,67};
+    // uint8_t k[]= {0};
+    // enc(p,k,sizeof(k));
 }
 int main()
 {
