42b897ef94ad8b4249a7b01cba0a368c6c64bb17
n132 <xm2146@nyu.edu>
2022-07-17 21:03:32 -0400

    Init Dockerizer

--- how2pwn/Dockerize/setup.py
+++ how2pwn/Dockerize/setup.py
@@ -4,14 +4,18 @@ import subprocess
 def call(cmd,cwd):
     return subprocess.check_call(cmd,shell=True,cwd=cwd)
     
-def init_chal_dir(stage,dir):
-    call("cp ../public/ctf.xinetd .",dir)
-    call("cp ../public/Dockerfile .",dir)
-    call("cp ../public/docker-compose.yml .",dir)
-    call("cp ../public/start.sh .",dir)
-    bin_dir = dir / "bin"
-    call(f"cp ../public/bin/OtherStages/{stage} .",bin_dir)
-    call(f"cp ../public/bin/OtherStages/{stage} .",bin_dir)
+def init_chal_dir(stage,cwd):
+    call(f"cp ../public/ctf.xinetd ./{stage}",cwd)
+    call(f"cp ../public/Dockerfile ./{stage}",cwd)
+    call(f"cp ../public/docker-compose.yml ./{stage}",cwd)
+    call(f"cp ../public/start.sh ./{stage}",cwd)
+    bin_dir = cwd / stage / "bin"
+    bin_dir.mkdir()
+    call(f"cp ../public/bin/OtherStages/{stage} ./{stage}/bin/",cwd)
+    call(f"cp ../chal/{stage}/flag ./{stage}/bin/",cwd)
+    if(Path(f"../chal/{stage}/ticket").exists()):
+        call(f"cp ../chal/{stage}/ticket ./{stage}/bin/",cwd)
+    
 def chal1():
     work_dir = Path(".") / "chal1"
     
@@ -21,6 +25,9 @@ def chal1():
         print("[-] Fail to mkdir")
         exit(1)
     
+    init_chal_dir("chal1",Path("."))
+    
+    
     
     
 
@@ -29,6 +36,6 @@ def clean():
 
 
 if __name__ == "__main__":
-    init_chal_dir();
-    # chal1()
+    
+    chal1()
 #    clean()
